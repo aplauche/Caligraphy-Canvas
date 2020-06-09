d0d9b4d0804ac3c390124b34e1b5b5b1f33158d1
Anton Plauche <antonplauche92@gmail.com>
2020-06-08 21:56:33 -0500

    added colors

--- 8-Canvas/app.js
+++ 8-Canvas/app.js
@@ -2,10 +2,13 @@ const canvas = document.getElementById('draw')
 
 const ctx = canvas.getContext('2d')
 
+const canvasColors = document.querySelectorAll('.bg-color')
+const penColors = document.querySelectorAll('.pen-color')
+
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-ctx.strokeStyle = '#BADA55'
+ctx.strokeStyle = '#222'
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
 ctx.lineWidth = 50
@@ -15,7 +18,7 @@ let isDrawing = false;
 let lastX = 0
 let lastY = 0
 let hue = 0
-let width = 5
+let width = 10
 let direction = true
 
 let compareX;
@@ -24,7 +27,7 @@ let compareY;
 function draw(e) {
     if(!isDrawing) return;
     console.log(e)
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+    // ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
     ctx.lineWidth = width
     ctx.beginPath();
     ctx.moveTo(lastX, lastY)
@@ -35,16 +38,31 @@ function draw(e) {
     hue++
     if(lastY > compareY){
         if(width < 25){
-            width++
+            width+=2
         }     
     } else if (lastY < compareY) {
-        if (width > 5){
-            width--
+        if (width > 10){
+            width-=2
         }
     }
     compareX = lastX
     compareY = lastY
 
+}
+
+function changeCanvasColor(e) {
+    canvas.style.backgroundColor = e.target.getAttribute('data-bg')
+    console.log(e.target)
+}
+
+function changePenColor(e) {
+    ctx.strokeStyle = e.target.getAttribute('data-pen')
+    penColors.forEach(color => {
+        color.style.height = '50px'
+    })
+    e.target.style.height = '60px'
+
+    console.log(e.target)
 }
 
 
@@ -58,3 +76,13 @@ canvas.addEventListener('mousedown', (e)=> {
 })
 canvas.addEventListener('mouseup', ()=> isDrawing = false)
 canvas.addEventListener('mouseout', ()=> isDrawing = false)
+
+canvasColors.forEach(color => {
+    color.style.backgroundColor = color.getAttribute('data-bg')
+    color.addEventListener('click', changeCanvasColor)
+})
+
+penColors.forEach(color => {
+    color.style.backgroundColor = color.getAttribute('data-pen')
+    color.addEventListener('click', changePenColor)
+})
